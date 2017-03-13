77e53100592e51254c96974120194b74471356c9
aboumekh <aboubakr.mekhatria@gmail.com>
2017-03-13 13:07:12 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,18 +80,17 @@ function Recordtimer(audioRecorder){
 			if (audioRecorder){
 			//to record time
 			var Start_time = new Date();
-			var seconds = math.round(Start_time/1000);
-			var minutes = math.round(seconds/60);
-			var hours = math.round(minutes /60);
-			document.getElementById("Recordingtime").innerHTML =hours.minutes.seconds;
 
 			}
-			
-			
-			
-		//To stop recording
-		//	var end_time = new Date();
-
+			else {
+			//To stop recording
+		    var end_time = new Date();
+			var recordingtime = end_time - Start_time;
+			var seconds = Math.round(Start_time/1000);
+			var minutes = Math.round(seconds/60);
+			var hours = Math.round(minutes /60);
+			document.getElementById("Recordingtime").innerHTML= (hours":"minutes":"seconds);
+			}
 	
 	
 }
